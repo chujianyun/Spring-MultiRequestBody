49280c40be4975c53460f5c0e543893a784763bb
mingmingruyue <liuwangyangedu@163.com>
2018-08-29 19:07:10 +0800

    修改WebConfig配置

--- src/main/java/com/chujianyun/config/WebConfig.java
+++ src/main/java/com/chujianyun/config/WebConfig.java
@@ -6,27 +6,27 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.nio.charset.Charset;
 import java.util.List;
 
 /**
- * 增强Controller方法注解，实现类似多个@RequestBody功能
+ * webmvc配置
  * @author 刘汪洋
  * @date 2018/08/27
  */
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        // 添加MultiRequestBody参数解析器
         argumentResolvers.add(new MultiRequestBodyArgumentResolver());
     }
 
     @Bean
     public HttpMessageConverter<String> responseBodyConverter() {
+        // 解决中文乱码问题
         return new StringHttpMessageConverter(Charset.forName("UTF-8"));
     }
 
