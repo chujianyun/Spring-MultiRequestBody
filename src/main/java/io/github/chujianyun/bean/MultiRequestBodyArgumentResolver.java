d819c22027c9c08a2771191dc435156c09426e36
liuwangyang <liuwangyang@corp.netease.com>
2019-02-07 15:05:46 +0800

    [fix]当list参数为空时，parameterType.newInstance会导致异常

--- src/main/java/io/github/chujianyun/bean/MultiRequestBodyArgumentResolver.java
+++ src/main/java/io/github/chujianyun/bean/MultiRequestBodyArgumentResolver.java
@@ -1,6 +1,7 @@
 package io.github.chujianyun.bean;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 import io.github.chujianyun.annotation.MultiRequestBody;
 import org.apache.commons.io.IOUtils;
@@ -36,207 +37,213 @@ import java.util.Set;
  */
 public class MultiRequestBodyArgumentResolver implements HandlerMethodArgumentResolver {
 
-    private static final String JSONBODY_ATTRIBUTE = "JSON_REQUEST_BODY";
+  private static final String JSONBODY_ATTRIBUTE = "JSON_REQUEST_BODY";
 
-    /**
-     * 设置支持的方法参数类型
-     *
-     * @param parameter 方法参数
-     * @return 支持的类型
-     */
-    @Override
-    public boolean supportsParameter(MethodParameter parameter) {
-        // 支持带@MultiRequestBody注解的参数
-        return parameter.hasParameterAnnotation(MultiRequestBody.class);
+  /**
+   * 设置支持的方法参数类型
+   *
+   * @param parameter 方法参数
+   * @return 支持的类型
+   */
+  @Override
+  public boolean supportsParameter(MethodParameter parameter) {
+    // 支持带@MultiRequestBody注解的参数
+    return parameter.hasParameterAnnotation(MultiRequestBody.class);
+  }
+
+  /**
+   * 参数解析，利用fastjson
+   * 注意：非基本类型返回null会报空指针异常，要通过反射或者JSON工具类创建一个空对象
+   */
+  @Override
+  public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+    String jsonBody = getRequestBody(webRequest);
+
+    JSONObject jsonObject = JSON.parseObject(jsonBody);
+    // 根据@MultiRequestBody注解value作为json解析的key
+    MultiRequestBody parameterAnnotation = parameter.getParameterAnnotation(MultiRequestBody.class);
+    //注解的value是JSON的key
+    String key = parameterAnnotation.value();
+    Object value;
+    // 如果@MultiRequestBody注解没有设置value，则取参数名FrameworkServlet作为json解析的key
+    if (StringUtils.isNotEmpty(key)) {
+      value = jsonObject.get(key);
+      // 如果设置了value但是解析不到，报错
+      if (value == null && parameterAnnotation.required()) {
+        throw new IllegalArgumentException(String.format("required param %s is not present", key));
+      }
+    } else {
+      // 注解为设置value则用参数名当做json的key
+      key = parameter.getParameterName();
+      value = jsonObject.get(key);
     }
 
-    /**
-     * 参数解析，利用fastjson
-     * 注意：非基本类型返回null会报空指针异常，要通过反射或者JSON工具类创建一个空对象
-     */
-    @Override
-    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-
-        String jsonBody = getRequestBody(webRequest);
-
-        JSONObject jsonObject = JSON.parseObject(jsonBody);
-        // 根据@MultiRequestBody注解value作为json解析的key
-        MultiRequestBody parameterAnnotation = parameter.getParameterAnnotation(MultiRequestBody.class);
-        //注解的value是JSON的key
-        String key = parameterAnnotation.value();
-        Object value;
-        // 如果@MultiRequestBody注解没有设置value，则取参数名FrameworkServlet作为json解析的key
-        if (StringUtils.isNotEmpty(key)) {
-            value = jsonObject.get(key);
-            // 如果设置了value但是解析不到，报错
-            if (value == null && parameterAnnotation.required()) {
-                throw new IllegalArgumentException(String.format("required param %s is not present", key));
-            }
-        } else {
-            // 注解为设置value则用参数名当做json的key
-            key = parameter.getParameterName();
-            value = jsonObject.get(key);
-        }
-
-        // 获取的注解后的类型 Long
-        Class<?> parameterType = parameter.getParameterType();
-        // 通过注解的value或者参数名解析，能拿到value进行解析
-        if (value != null) {
-            //基本类型
-            if (parameterType.isPrimitive()) {
-                return parsePrimitive(parameterType.getName(), value);
-            }
-            // 基本类型包装类
-            if (isBasicDataTypes(parameterType)) {
-                return parseBasicTypeWrapper(parameterType, value);
-                // 字符串类型
-            } else if (parameterType == String.class) {
-                return value.toString();
-            }
-            // 其他复杂对象
-            return JSON.parseObject(value.toString(), parameterType);
-        }
-
-        // 解析不到则将整个json串解析为当前参数类型
-        if (isBasicDataTypes(parameterType)) {
-            if (parameterAnnotation.required()) {
-                throw new IllegalArgumentException(String.format("required param %s is not present", key));
-            } else {
-                return null;
-            }
-        }
-
-        Object result = parameterType.newInstance();
-        // 非基本类型，不允许解析所有字段，返回null
-        if (!parameterAnnotation.parseAllFields()) {
-            // 如果是必传参数抛异常
-            if (parameterAnnotation.required()) {
-                throw new IllegalArgumentException(String.format("required param %s is not present", key));
-            }
-            // 否则返回空对象
-            return result;
-        }
-        // 非基本类型，允许解析，将外层属性解析
-        result = JSON.parseObject(jsonObject.toString(), parameterType);
-        // 如果非必要参数直接返回，否则如果没有一个属性有值则报错
-        if (!parameterAnnotation.required()) {
-            return result;
-        } else {
-            boolean haveValue = false;
-            Field[] declaredFields = parameterType.getDeclaredFields();
-            for (Field field : declaredFields) {
-                field.setAccessible(true);
-                if (field.get(result) != null) {
-                    haveValue = true;
-                    break;
-                }
-            }
-            if (!haveValue) {
-                throw new IllegalArgumentException(String.format("required param %s is not present", key));
-            }
-            return result;
-        }
+    // 获取的注解后的类型 Long
+    Class<?> parameterType = parameter.getParameterType();
+    // 通过注解的value或者参数名解析，能拿到value进行解析
+    if (value != null) {
+      //基本类型
+      if (parameterType.isPrimitive()) {
+        return parsePrimitive(parameterType.getName(), value);
+      }
+      // 基本类型包装类
+      if (isBasicDataTypes(parameterType)) {
+        return parseBasicTypeWrapper(parameterType, value);
+        // 字符串类型
+      } else if (parameterType == String.class) {
+        return value.toString();
+      }
+      // 其他复杂对象
+      return JSON.parseObject(value.toString(), parameterType);
     }
 
-    /**
-     * 基本类型解析
-     */
-    private Object parsePrimitive(String parameterTypeName, Object value) {
-        final String booleanTypeName = "boolean";
-        if (booleanTypeName.equals(parameterTypeName)) {
-            return Boolean.valueOf(value.toString());
-        }
-        final String intTypeName = "int";
-        if (intTypeName.equals(parameterTypeName)) {
-            return Integer.valueOf(value.toString());
-        }
-        final String charTypeName = "char";
-        if (charTypeName.equals(parameterTypeName)) {
-            return value.toString().charAt(0);
-        }
-        final String shortTypeName = "short";
-        if (shortTypeName.equals(parameterTypeName)) {
-            return Short.valueOf(value.toString());
-        }
-        final String longTypeName = "long";
-        if (longTypeName.equals(parameterTypeName)) {
-            return Long.valueOf(value.toString());
-        }
-        final String floatTypeName = "float";
-        if (floatTypeName.equals(parameterTypeName)) {
-            return Float.valueOf(value.toString());
-        }
-        final String doubleTypeName = "double";
-        if (doubleTypeName.equals(parameterTypeName)) {
-            return Double.valueOf(value.toString());
-        }
-        final String byteTypeName = "byte";
-        if (byteTypeName.equals(parameterTypeName)) {
-            return Byte.valueOf(value.toString());
-        }
+    // 解析不到则将整个json串解析为当前参数类型
+    if (isBasicDataTypes(parameterType)) {
+      if (parameterAnnotation.required()) {
+        throw new IllegalArgumentException(String.format("required param %s is not present", key));
+      } else {
         return null;
+      }
     }
 
-    /**
-     * 基本类型包装类解析
-     */
-    private Object parseBasicTypeWrapper(Class<?> parameterType, Object value) {
-        if (Number.class.isAssignableFrom(parameterType)) {
-            Number number = (Number) value;
-            if (parameterType == Integer.class) {
-                return number.intValue();
-            } else if (parameterType == Short.class) {
-                return number.shortValue();
-            } else if (parameterType == Long.class) {
-                return number.longValue();
-            } else if (parameterType == Float.class) {
-                return number.floatValue();
-            } else if (parameterType == Double.class) {
-                return number.doubleValue();
-            } else if (parameterType == Byte.class) {
-                return number.byteValue();
-            }
-        } else if (parameterType == Boolean.class) {
-            return value.toString();
-        } else if (parameterType == Character.class) {
-            return value.toString().charAt(0);
+    // 非基本类型，不允许解析所有字段，必备参数则报错，非必备参数则返回null
+    if (!parameterAnnotation.parseAllFields()) {
+      // 如果是必传参数抛异常
+      if (parameterAnnotation.required()) {
+        throw new IllegalArgumentException(String.format("required param %s is not present", key));
+      }
+      // 否则返回null
+      return null;
+    }
+    // 非基本类型，允许解析，将外层属性解析
+    Object result;
+    try {
+      result = JSON.parseObject(jsonObject.toString(), parameterType);
+    } catch (JSONException jsonException) {
+      // TODO:: 异常处理返回null是否合理？
+      result = null;
+    }
+
+    // 如果非必要参数直接返回，否则如果没有一个属性有值则报错
+    if (!parameterAnnotation.required()) {
+      return result;
+    } else {
+      boolean haveValue = false;
+      Field[] declaredFields = parameterType.getDeclaredFields();
+      for (Field field : declaredFields) {
+        field.setAccessible(true);
+        if (field.get(result) != null) {
+          haveValue = true;
+          break;
         }
-        return null;
+      }
+      if (!haveValue) {
+        throw new IllegalArgumentException(String.format("required param %s is not present", key));
+      }
+      return result;
     }
+  }
 
-    /**
-     * 判断是否为基本数据类型包装类
-     */
-    private boolean isBasicDataTypes(Class clazz) {
-        Set<Class> classSet = new HashSet<>();
-        classSet.add(Integer.class);
-        classSet.add(Long.class);
-        classSet.add(Short.class);
-        classSet.add(Float.class);
-        classSet.add(Double.class);
-        classSet.add(Boolean.class);
-        classSet.add(Byte.class);
-        classSet.add(Character.class);
-        return classSet.contains(clazz);
+  /**
+   * 基本类型解析
+   */
+  private Object parsePrimitive(String parameterTypeName, Object value) {
+    final String booleanTypeName = "boolean";
+    if (booleanTypeName.equals(parameterTypeName)) {
+      return Boolean.valueOf(value.toString());
     }
-
-    /**
-     * 获取请求体JSON字符串
-     */
-    private String getRequestBody(NativeWebRequest webRequest) {
-        HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
-
-        // 有就直接获取
-        String jsonBody = (String) webRequest.getAttribute(JSONBODY_ATTRIBUTE, NativeWebRequest.SCOPE_REQUEST);
-        // 没有就从请求中读取
-        if (jsonBody == null) {
-            try {
-                jsonBody = IOUtils.toString(servletRequest.getReader());
-                webRequest.setAttribute(JSONBODY_ATTRIBUTE, jsonBody, NativeWebRequest.SCOPE_REQUEST);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return jsonBody;
+    final String intTypeName = "int";
+    if (intTypeName.equals(parameterTypeName)) {
+      return Integer.valueOf(value.toString());
     }
+    final String charTypeName = "char";
+    if (charTypeName.equals(parameterTypeName)) {
+      return value.toString().charAt(0);
+    }
+    final String shortTypeName = "short";
+    if (shortTypeName.equals(parameterTypeName)) {
+      return Short.valueOf(value.toString());
+    }
+    final String longTypeName = "long";
+    if (longTypeName.equals(parameterTypeName)) {
+      return Long.valueOf(value.toString());
+    }
+    final String floatTypeName = "float";
+    if (floatTypeName.equals(parameterTypeName)) {
+      return Float.valueOf(value.toString());
+    }
+    final String doubleTypeName = "double";
+    if (doubleTypeName.equals(parameterTypeName)) {
+      return Double.valueOf(value.toString());
+    }
+    final String byteTypeName = "byte";
+    if (byteTypeName.equals(parameterTypeName)) {
+      return Byte.valueOf(value.toString());
+    }
+    return null;
+  }
+
+  /**
+   * 基本类型包装类解析
+   */
+  private Object parseBasicTypeWrapper(Class<?> parameterType, Object value) {
+    if (Number.class.isAssignableFrom(parameterType)) {
+      Number number = (Number) value;
+      if (parameterType == Integer.class) {
+        return number.intValue();
+      } else if (parameterType == Short.class) {
+        return number.shortValue();
+      } else if (parameterType == Long.class) {
+        return number.longValue();
+      } else if (parameterType == Float.class) {
+        return number.floatValue();
+      } else if (parameterType == Double.class) {
+        return number.doubleValue();
+      } else if (parameterType == Byte.class) {
+        return number.byteValue();
+      }
+    } else if (parameterType == Boolean.class) {
+      return value.toString();
+    } else if (parameterType == Character.class) {
+      return value.toString().charAt(0);
+    }
+    return null;
+  }
+
+  /**
+   * 判断是否为基本数据类型包装类
+   */
+  private boolean isBasicDataTypes(Class clazz) {
+    Set<Class> classSet = new HashSet<>();
+    classSet.add(Integer.class);
+    classSet.add(Long.class);
+    classSet.add(Short.class);
+    classSet.add(Float.class);
+    classSet.add(Double.class);
+    classSet.add(Boolean.class);
+    classSet.add(Byte.class);
+    classSet.add(Character.class);
+    return classSet.contains(clazz);
+  }
+
+  /**
+   * 获取请求体JSON字符串
+   */
+  private String getRequestBody(NativeWebRequest webRequest) {
+    HttpServletRequest servletRequest = webRequest.getNativeRequest(HttpServletRequest.class);
+
+    // 有就直接获取
+    String jsonBody = (String) webRequest.getAttribute(JSONBODY_ATTRIBUTE, NativeWebRequest.SCOPE_REQUEST);
+    // 没有就从请求中读取
+    if (jsonBody == null) {
+      try {
+        jsonBody = IOUtils.toString(servletRequest.getReader());
+        webRequest.setAttribute(JSONBODY_ATTRIBUTE, jsonBody, NativeWebRequest.SCOPE_REQUEST);
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return jsonBody;
+  }
 }
--- src/main/java/io/github/chujianyun/controller/DemoController.java
+++ src/main/java/io/github/chujianyun/controller/DemoController.java
@@ -8,8 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 /**
  * Controller Demo
+ *
  * @author Wangyang Liu
  * @date 2018/08/27
  */
@@ -18,81 +21,89 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class DemoController {
 
 
+  @RequestMapping("/testStr")
+  @ResponseBody
+  public String multiRequestBodyDemo1(@MultiRequestBody String test1, @MultiRequestBody String test2) {
 
-    @RequestMapping("/testStr")
-    @ResponseBody
-    public String multiRequestBodyDemo1(@MultiRequestBody String test1,@MultiRequestBody  String test2) {
+    System.out.println(test1 + "-->" + test2);
+    return "";
+  }
 
-        System.out.println(test1 +"-->" + test2);
-        return "";
-    }
+  @RequestMapping("/testChar")
+  @ResponseBody
+  public String multiRequestBodyDemo1(@MultiRequestBody char id) {
 
-    @RequestMapping("/testChar")
-    @ResponseBody
-    public String multiRequestBodyDemo1(@MultiRequestBody char id) {
+    System.out.println(id);
+    return "";
+  }
 
-        System.out.println(id);
-        return "";
-    }
-    @RequestMapping("/demo")
-    @ResponseBody
-    public String multiRequestBodyDemo1(@MultiRequestBody Dog dog, @MultiRequestBody User user) {
-        System.out.println(dog.toString() + user.toString());
-        return dog.toString() + ";" + user.toString();
-    }
-
-
-    @RequestMapping("/demo2")
-    @ResponseBody
-    public String multiRequestBodyDemo2(@MultiRequestBody("dog") Dog dog, @MultiRequestBody User user) {
-        System.out.println(dog.toString() + user.toString());
-        return dog.toString() + ";" + user.toString();
-    }
-
-    @RequestMapping("/demo3")
-    @ResponseBody
-    public String multiRequestBodyDemo3(@MultiRequestBody("dog") Dog dog, @MultiRequestBody("user") User user) {
-        System.out.println(dog.toString() + user.toString());
-        return dog.toString() + ";" + user.toString();
-    }
-
-    @RequestMapping("/demo4")
-    @ResponseBody
-    public String multiRequestBodyDemo4(@MultiRequestBody("dog") Dog dog, @MultiRequestBody Integer age) {
-        System.out.println(dog.toString() + age.toString());
-        return dog.toString() + ";age属性为："+age.toString();
-    }
+  @RequestMapping("/demo")
+  @ResponseBody
+  public String multiRequestBodyDemo1(@MultiRequestBody Dog dog, @MultiRequestBody User user) {
+    System.out.println(dog.toString() + user.toString());
+    return dog.toString() + ";" + user.toString();
+  }
 
 
-    @RequestMapping("/demo5")
-    @ResponseBody
-    public String multiRequestBodyDemo5(@MultiRequestBody("color") String color, @MultiRequestBody("age") Integer age) {
-        return "color="+color + "; age=" + age;
-    }
+  @RequestMapping("/demo2")
+  @ResponseBody
+  public String multiRequestBodyDemo2(@MultiRequestBody("dog") Dog dog, @MultiRequestBody User user) {
+    System.out.println(dog.toString() + user.toString());
+    return dog.toString() + ";" + user.toString();
+  }
 
-    @RequestMapping("/demo6")
-    @ResponseBody
-    public String multiRequestBodyDemo6(@MultiRequestBody("dog") Dog dog, @MultiRequestBody Integer age) {
-        System.out.println(dog.toString() + age.toString());
-        return dog.toString() + ";age属性为："+age.toString();
-    }
+  @RequestMapping("/demo3")
+  @ResponseBody
+  public String multiRequestBodyDemo3(@MultiRequestBody("dog") Dog dog, @MultiRequestBody("user") User user) {
+    System.out.println(dog.toString() + user.toString());
+    return dog.toString() + ";" + user.toString();
+  }
+
+  @RequestMapping("/demo4")
+  @ResponseBody
+  public String multiRequestBodyDemo4(@MultiRequestBody("dog") Dog dog, @MultiRequestBody Integer age) {
+    System.out.println(dog.toString() + age.toString());
+    return dog.toString() + ";age属性为：" + age.toString();
+  }
 
 
-    @RequestMapping("/demo7")
-    @ResponseBody
-    public String multiRequestBodyDemo7(@MultiRequestBody(required = false) Dog dog, @MultiRequestBody("age") Integer age) {
-        return "dog="+dog + "; age=" + age;
-    }
+  @RequestMapping("/demo5")
+  @ResponseBody
+  public String multiRequestBodyDemo5(@MultiRequestBody("color") String color, @MultiRequestBody("age") Integer age) {
+    return "color=" + color + "; age=" + age;
+  }
+
+  @RequestMapping("/demo6")
+  @ResponseBody
+  public String multiRequestBodyDemo6(@MultiRequestBody("dog") Dog dog, @MultiRequestBody Integer age) {
+    System.out.println(dog.toString() + age.toString());
+    return dog.toString() + ";age属性为：" + age.toString();
+  }
 
 
-    @RequestMapping("/demo10")
-    @ResponseBody
-    public String multiRequestBodyDemo10( @MultiRequestBody(parseAllFields = false,required = false) Dog dog) {
-        return dog.toString();
-    }
-    @RequestMapping("/demo99")
-    @ResponseBody
-    public String multiRequestBodyDemo99( @MultiRequestBody(parseAllFields = false,required = false) Character demo) {
-        return demo.toString();
-    }
+  @RequestMapping("/demo7")
+  @ResponseBody
+  public String multiRequestBodyDemo7(@MultiRequestBody(required = false) Dog dog, @MultiRequestBody("age") Integer age) {
+    return "dog=" + dog + "; age=" + age;
+  }
+
+
+  @RequestMapping("/demo10")
+  @ResponseBody
+  public String multiRequestBodyDemo10(@MultiRequestBody(parseAllFields = false, required = false) Dog dog) {
+    return dog.toString();
+  }
+
+  @RequestMapping("/demo99")
+  @ResponseBody
+  public String multiRequestBodyDemo99(@MultiRequestBody(parseAllFields = false, required = false) Character demo) {
+    return demo.toString();
+  }
+
+  @RequestMapping("/testList")
+  @ResponseBody
+  public String multiRequestBodyDemo1(@MultiRequestBody List test, @MultiRequestBody String str) {
+
+    return test.toString() + str;
+  }
 }
